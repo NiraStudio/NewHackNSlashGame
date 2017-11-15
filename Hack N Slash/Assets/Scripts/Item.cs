0723867ff635e0bda623ebc13dc85b236fef2fb3
Amirfery <Amirferyg@gmail.com>
2017-11-16 01:18:28 +0330

    Item Added

---
+++ Hack N Slash/Assets/Scripts/Item.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item :MonoBehaviour {
+    [SerializeField]
+    float hitPoint;
+    [SerializeField]
+    float damge;
+    [SerializeField]
+    ItemType itemType;
+
+    void Start()
+    {
+        switch (itemType)
+        {
+            case ItemType.Weapon:
+                break;
+            case ItemType.HandArmors:
+                break;
+            case ItemType.LegArmors:
+                break;
+            case ItemType.HeadArmor:
+                break;
+            case ItemType.BodyArmor:
+                break;
+
+        }
+    }
+
+}
+public enum ItemType
+{
+    Weapon,HeadArmor,HandArmors,LegArmors,BodyArmor
+}
